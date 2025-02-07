6a56d3521c036d86643a0729d2a4b1ef8412c887
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-02-07 10:57:30 +0100

    Add example assets

--- gift_box/lib/static/resources/assets.dart
+++ gift_box/lib/static/resources/assets.dart
@@ -1,3 +1,12 @@
+import 'package:gift_box/domain/models/asset.dart';
+
 final class Assets {
   const Assets._();
+
+  static final example1 = Asset('assets/images/example_1.webp');
+  static final example2 = Asset('assets/images/example_2.webp');
+  static final example3 = Asset('assets/images/example_3.webp');
+  static final example4 = Asset('assets/images/example_4.webp');
+  static final example5 = Asset('assets/images/example_5.webp');
+  static final example6 = Asset('assets/images/example_6.webp');
 }
