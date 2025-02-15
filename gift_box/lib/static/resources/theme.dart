f628cac183dcf9cf8a486c3f3d62737bb55b424c
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-02-15 20:52:47 +0100

    Enable updated ProgressIndicator style

--- gift_box/lib/static/resources/theme.dart
+++ gift_box/lib/static/resources/theme.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 final class CustomTheme {
   const CustomTheme._();
 
-  static final ThemeData light = ThemeData.light();
-  static final ThemeData dark = ThemeData.dark();
+  static final ThemeData light = ThemeData.light().copyWith(
+    // ignore: deprecated_member_use, since we want to use the updated version.
+    progressIndicatorTheme: const ProgressIndicatorThemeData(year2023: false),
+  );
+  static final ThemeData dark = ThemeData.dark().copyWith(
+    // ignore: deprecated_member_use, since we want to use the updated version.
+    progressIndicatorTheme: const ProgressIndicatorThemeData(year2023: false),
+  );
 }
