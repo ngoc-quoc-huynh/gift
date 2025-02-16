485ad9ed8ae7dc34b3170ab6d0ca135737d31912
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-02-16 15:41:09 +0100

    Add tolerance to golden test

--- gift_box/test/flutter_test_config.dart
+++ gift_box/test/flutter_test_config.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:alchemist/alchemist.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gift_box/static/resources/theme.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
@@ -12,5 +14,48 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
           enabled: !bool.fromEnvironment('CI'),
         ),
       ),
-      run: () => testMain(),
+      run: () {
+        final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
+        goldenFileComparator = _LocalFileComparatorWithThreshold(
+          Uri.parse('$testUrl/test.dart'),
+          0.001,
+        );
+
+        return testMain();
+      },
     );
+
+class _LocalFileComparatorWithThreshold extends LocalFileComparator {
+  _LocalFileComparatorWithThreshold(super.testFile, this.threshold)
+      : assert(
+          threshold >= 0 && threshold <= 1,
+          'Threshold must be between 0 and 1! ',
+        );
+
+  final double threshold;
+
+  @override
+  Future<bool> compare(Uint8List imageBytes, Uri golden) async {
+    final result = await GoldenFileComparator.compareLists(
+      imageBytes,
+      await getGoldenBytes(golden),
+    );
+
+    if (!result.passed && result.diffPercent <= threshold) {
+      debugPrint(
+        'A difference of ${result.diffPercent * 100}% was found, but it is '
+        'acceptable since it is not greater than the threshold of '
+        '${threshold * 100}%.',
+      );
+
+      return true;
+    }
+
+    if (!result.passed) {
+      final error = await generateFailureOutput(result, golden, basedir);
+      throw FlutterError(error);
+    }
+
+    return result.passed;
+  }
+}
