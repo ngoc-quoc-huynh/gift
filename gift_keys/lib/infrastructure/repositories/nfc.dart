29763b6fee441886ae6266894128ff6d881863cc
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-03-22 11:40:16 +0100

    Add logs to NfcRepository

--- gift_keys/lib/infrastructure/repositories/nfc.dart
+++ gift_keys/lib/infrastructure/repositories/nfc.dart
@@ -1,18 +1,27 @@
 import 'package:gift_keys/domain/interfaces/nfc.dart';
 import 'package:gift_keys/domain/models/nfc_command.dart' as domain;
+import 'package:gift_keys/domain/utils/mixins/logger.dart';
 import 'package:gift_keys/infrastructure/dtos/nfc/command.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
-final class NfcRepository implements NfcApi {
+final class NfcRepository with LoggerMixin implements NfcApi {
   const NfcRepository();
 
   static final _instance = NfcManager();
 
   @override
-  Future<bool> isEnabled() => _instance.isNfcEnabled();
+  Future<bool> isEnabled() async {
+    final isEnabled = await _instance.isNfcEnabled();
+    logInfo('Checked if NFC is enabled: $isEnabled');
+
+    return isEnabled;
+  }
 
   @override
-  Stream<String> startDiscovery() => _instance.startDiscovery();
+  Stream<String> startDiscovery() => _instance.startDiscovery().map((tag) {
+    logInfo('Discovered NFC tag: $tag');
+    return tag;
+  });
 
   @override
   Future<bool> sendCommand(domain.NfcCommand nfcCommand) async {
@@ -20,9 +29,12 @@ final class NfcRepository implements NfcApi {
       CommandExtension.fromDomain(nfcCommand),
     );
 
-    return switch (response) {
+    final result = switch (response) {
       ApduResponse.ok => true,
       _ => false,
     };
+    logInfo('Sent NFC command with response: $result');
+
+    return result;
   }
 }
