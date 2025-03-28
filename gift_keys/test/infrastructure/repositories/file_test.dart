58db8b882f07c35fb4ca0273e0afb5fb7b71029a
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-03-28 18:40:22 +0100

    Add tests for FileRepository

---
+++ gift_keys/test/infrastructure/repositories/file_test.dart
@@ -0,0 +1,196 @@
+import 'package:file/file.dart';
+import 'package:file/memory.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gift_keys/domain/interfaces/logger.dart';
+import 'package:gift_keys/infrastructure/repositories/file.dart';
+import 'package:gift_keys/injector.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../mocks.dart';
+
+void main() {
+  final fileSystem = MemoryFileSystem();
+  final imagePicker = MockImagePicker();
+  final loggerApi = MockLoggerApi();
+  final repository = FileRepository(imagePicker);
+
+  setUpAll(() {
+    Injector.instance
+      ..registerSingleton<Directory>(
+        fileSystem.directory('app'),
+        instanceName: 'appDir',
+      )
+      ..registerSingleton<Directory>(
+        fileSystem.directory('tmp'),
+        instanceName: 'tmpDir',
+      )
+      ..registerSingleton<FileSystem>(fileSystem)
+      ..registerSingleton<LoggerApi>(loggerApi);
+    registerFallbackValue(PlatformException(code: 'code'));
+    registerFallbackValue(StackTrace.empty);
+  });
+
+  tearDownAll(Injector.instance.reset);
+
+  group('pickImageFromGallery', () {
+    test('returns correctly when file is picked.', () async {
+      final expected = fileSystem.file('test.jpg');
+      when(
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+      ).thenAnswer((_) async => XFile('test.jpg'));
+
+      final result = await repository.pickImageFromGallery();
+
+      expect(result, isNotNull);
+      expect(result!.path, expected.path);
+
+      final verifications = verifyInOrder([
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+        () => loggerApi.logInfo(any()),
+      ]);
+      for (final verification in verifications) {
+        verification.called(1);
+      }
+    });
+
+    test('returns correctly when no file is picked.', () async {
+      when(
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+      ).thenAnswer((_) async => null);
+
+      final result = await repository.pickImageFromGallery();
+
+      expect(result, isNull);
+
+      final verifications = verifyInOrder([
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+        () => loggerApi.logInfo(any()),
+      ]);
+      for (final verification in verifications) {
+        verification.called(1);
+      }
+    });
+
+    test('returns correctly PlatformException is thrown.', () async {
+      when(
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+      ).thenThrow(PlatformException(code: 'code'));
+
+      final result = await repository.pickImageFromGallery();
+
+      expect(result, isNull);
+
+      final verifications = verifyInOrder([
+        () => imagePicker.pickImage(source: ImageSource.gallery),
+        () => loggerApi.logException(
+          any(),
+          exception: any(named: 'exception'),
+          stackTrace: any(named: 'stackTrace'),
+        ),
+      ]);
+      for (final verification in verifications) {
+        verification.called(1);
+      }
+    });
+  });
+
+  group('compressImage', () {
+    // TODO: Check how to mock FlutterImageCompress.compressAndGetFile.
+  });
+
+  group('moveFileToAppDir', () {
+    test('returns correctly when the paths are different.', () async {
+      final file = fileSystem.file('tmp/test.webp')
+        ..createSync(recursive: true);
+      addTearDown(
+        () => fileSystem.directory('app/images').delete(recursive: true),
+      );
+
+      final result = await repository.moveFileToAppDir(file.path, 1);
+
+      expect(result.path, 'app/images/1.webp');
+      verify(() => loggerApi.logInfo(any())).called(2);
+    });
+
+    test('returns correctly when the paths are equal.', () async {
+      final file = fileSystem.file('app/images/1.webp')
+        ..createSync(recursive: true);
+      final result = await repository.moveFileToAppDir(file.path, 1);
+
+      expect(result.path, 'app/images/1.webp');
+
+      final verifications = verifyInOrder([
+        () => loggerApi.logInfo(any()),
+        () => loggerApi.logWarning(any()),
+      ]);
+      for (final verification in verifications) {
+        verification.called(1);
+      }
+    });
+  });
+
+  group('loadImage', () {
+    test('returns correctly', () {
+      expect(repository.loadImage(1).path, 'app/images/1.webp');
+
+      verify(() => loggerApi.logInfo(any())).called(1);
+    });
+  });
+
+  group('deleteAllImages', () {
+    test('returns correctly when directory exists.', () async {
+      fileSystem.directory('app/images').createSync(recursive: true);
+      await expectLater(repository.deleteAllImages(), completes);
+
+      verify(() => loggerApi.logInfo(any())).called(1);
+    });
+
+    test('returns correctly when directory does not exist.', () async {
+      await expectLater(repository.deleteAllImages(), completes);
+
+      verify(() => loggerApi.logWarning(any())).called(1);
+    });
+  });
+
+  group('deleteImage', () {
+    test('returns correctly when file exists.', () async {
+      fileSystem.file('app/images/1.webp').createSync(recursive: true);
+      addTearDown(
+        () => fileSystem.directory('app/images').delete(recursive: true),
+      );
+
+      await expectLater(repository.deleteImage(1), completes);
+
+      verify(() => loggerApi.logInfo(any())).called(2);
+    });
+
+    test('returns correctly when file does not exist.', () async {
+      await expectLater(repository.deleteImage(1), completes);
+
+      final verifications = verifyInOrder([
+        () => loggerApi.logInfo(any()),
+        () => loggerApi.logWarning(any()),
+      ]);
+      for (final verification in verifications) {
+        verification.called(1);
+      }
+    });
+  });
+
+  group('clearCache', () {
+    test('returns correctly when tmp directory exists.', () async {
+      fileSystem.directory('tmp').createSync();
+      await expectLater(repository.clearCache(), completes);
+
+      verify(() => loggerApi.logInfo(any())).called(1);
+    });
+
+    test('returns correctly when tmp does not directory exist.', () async {
+      await expectLater(repository.clearCache(), completes);
+
+      verify(() => loggerApi.logWarning(any())).called(1);
+    });
+  });
+}
--- gift_keys/test/mocks.dart
+++ gift_keys/test/mocks.dart
@@ -1,6 +1,9 @@
 import 'package:gift_keys/domain/interfaces/logger.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:logger/logger.dart';
 import 'package:mocktail/mocktail.dart';
+
+final class MockImagePicker extends Mock implements ImagePicker {}
 
 final class MockLogger extends Mock implements Logger {}
 
