fa072293a43a0b85d003e2ad3cc6f03fcd2fdbc0
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-04-10 16:39:04 +0200

    Add tests for CustomSnackBar

---
+++ gift_keys/test/ui/widgets/snack_bar_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:gift_keys/ui/widgets/snack_bar.dart';
+
+import '../../utils.dart';
+
+void main() {
+  testGolden('renders error correctly.', (tester) async {
+    final widget = _TestWidget(
+      (context) => CustomSnackBar.showError(context, 'Error'),
+    );
+    await tester.pumpGoldenWidget(widget);
+    await tester.pumpAndSettle();
+    await expectGoldenFile('snack_bar_error', widget);
+  }, surfaceSize: const Size(200, 80));
+
+  testGolden('renders info correctly.', (tester) async {
+    final widget = _TestWidget(
+      (context) => CustomSnackBar.showInfo(context, 'Info'),
+    );
+    await tester.pumpGoldenWidget(widget);
+    await tester.pumpAndSettle();
+    await expectGoldenFile('snack_bar_info', widget);
+  }, surfaceSize: const Size(200, 80));
+
+  testGolden('renders success correctly.', (tester) async {
+    final widget = _TestWidget(
+      (context) => CustomSnackBar.showSuccess(context, 'Success'),
+    );
+    await tester.pumpGoldenWidget(widget);
+    await tester.pumpAndSettle();
+    await expectGoldenFile('snack_bar_success', widget);
+  }, surfaceSize: const Size(200, 80));
+}
+
+final class _TestWidget extends StatelessWidget {
+  const _TestWidget(this.showSnackBar);
+
+  final void Function(BuildContext) showSnackBar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        WidgetsBinding.instance.addPostFrameCallback(
+          (_) => showSnackBar(context),
+        );
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
