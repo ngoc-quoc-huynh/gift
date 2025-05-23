e7e67a496315512ab994e577d6f49343efbe9e6b
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-05-23 19:09:59 +0200

    Add NavigationBar to AwesomeShopCatalogPage

--- gift_box/lib/ui/pages/awesome_shop_catalog/page.dart
+++ gift_box/lib/ui/pages/awesome_shop_catalog/page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:gift_box/domain/models/route.dart';
+import 'package:gift_box/domain/utils/extensions/build_context.dart';
+import 'package:gift_box/injector.dart';
 
 class AwesomeShopCatalogPage extends StatelessWidget {
   const AwesomeShopCatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_translations.appBar),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: _translations.settingsTooltip,
+            onPressed: () => context.pushRoute(AppRoute.settings),
+          ),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Icons.local_offer_outlined),
+            label: _translations.navigationBar.specials,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.palette_outlined),
+            label: _translations.navigationBar.customizer,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.local_cafe_outlined),
+            label: _translations.navigationBar.equipment,
+          ),
+        ],
+      ),
+    );
   }
+
+  TranslationsPagesAwesomeShopCatalogEn get _translations =>
+      Injector.instance.translations.pages.awesomeShopCatalog;
 }
