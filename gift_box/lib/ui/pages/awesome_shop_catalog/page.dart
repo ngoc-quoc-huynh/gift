4eb1915a30b6575e71351153120c2ea22aac1aeb
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-05-25 09:57:13 +0200

    Add AwesomeShopCatalogPage

--- gift_box/lib/ui/pages/awesome_shop_catalog/page.dart
+++ gift_box/lib/ui/pages/awesome_shop_catalog/page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gift_box/domain/blocs/value/cubit.dart';
 import 'package:gift_box/domain/models/route.dart';
 import 'package:gift_box/domain/utils/extensions/build_context.dart';
 import 'package:gift_box/injector.dart';
@@ -8,32 +10,48 @@ class AwesomeShopCatalogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_translations.appBar),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: _translations.settingsTooltip,
-            onPressed: () => context.pushRoute(AppRoute.settings),
-          ),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(
-            icon: const Icon(Icons.local_offer_outlined),
-            label: _translations.navigationBar.specials,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.palette_outlined),
-            label: _translations.navigationBar.customizer,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.local_cafe_outlined),
-            label: _translations.navigationBar.equipment,
-          ),
-        ],
+    return BlocProvider<NavigationBarCubit>(
+      create: (_) => NavigationBarCubit(NavigationBarState.specials),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_translations.appBar),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: _translations.settingsTooltip,
+              onPressed: () => context.pushRoute(AppRoute.settings),
+            ),
+          ],
+        ),
+        bottomNavigationBar:
+            BlocBuilder<NavigationBarCubit, NavigationBarState>(
+              builder: (context, state) => NavigationBar(
+                selectedIndex: state.index,
+                onDestinationSelected: (index) => context
+                    .read<NavigationBarCubit>()
+                    .update(NavigationBarState.values[index]),
+                destinations: [
+                  NavigationDestination(
+                    icon: const Icon(Icons.local_offer_outlined),
+                    selectedIcon: const Icon(Icons.local_offer_rounded),
+                    label: _translations.navigationBar.specials,
+                    tooltip: _translations.navigationBar.specials,
+                  ),
+                  NavigationDestination(
+                    icon: const Icon(Icons.palette_outlined),
+                    selectedIcon: const Icon(Icons.palette_rounded),
+                    label: _translations.navigationBar.customizer,
+                    tooltip: _translations.navigationBar.customizer,
+                  ),
+                  NavigationDestination(
+                    icon: const Icon(Icons.local_cafe_outlined),
+                    selectedIcon: const Icon(Icons.local_cafe_rounded),
+                    label: _translations.navigationBar.equipment,
+                    tooltip: _translations.navigationBar.equipment,
+                  ),
+                ],
+              ),
+            ),
       ),
     );
   }
