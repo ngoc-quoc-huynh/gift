2898fcb909a1ce68406d86c1e7ecf7b1e855c553
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-05-25 09:43:47 +0200

    Add satisfactory assets

--- gift_box/lib/domain/models/asset.dart
+++ gift_box/lib/domain/models/asset.dart
@@ -1,6 +1,16 @@
 import 'package:path/path.dart';
 
 enum Asset {
+  // Awesome shop items
+  ada('awesome_shop/ada.webp'),
+  ficsitCoupon('awesome_shop/ficsit_coupon.webp'),
+  ficsitCoffeeCup('awesome_shop/ficsit_coffee_cup.webp'),
+  darkMode('awesome_shop/dark_mode.webp'),
+  germanDrive('awesome_shop/german_drive.webp'),
+  musicTape('awesome_shop/music_tape.webp'),
+  reset('awesome_shop/reset.webp'),
+
+  // Rive files
   gift('rive/gift.riv')
   ,
   satisfactory('rive/satisfactory.riv');
--- gift_box/test/domain/models/asset_test.dart
+++ gift_box/test/domain/models/asset_test.dart
@@ -5,6 +5,16 @@ void main() {
   test(
     'call returns correctly.',
     () {
+      expect(Asset.ada(), 'assets/awesome_shop/ada.webp');
+      expect(Asset.ficsitCoupon(), 'assets/awesome_shop/ficsit_coupon.webp');
+      expect(
+        Asset.ficsitCoffeeCup(),
+        'assets/awesome_shop/ficsit_coffee_cup.webp',
+      );
+      expect(Asset.darkMode(), 'assets/awesome_shop/dark_mode.webp');
+      expect(Asset.germanDrive(), 'assets/awesome_shop/german_drive.webp');
+      expect(Asset.musicTape(), 'assets/awesome_shop/music_tape.webp');
+      expect(Asset.reset(), 'assets/awesome_shop/reset.webp');
       expect(Asset.gift(), 'assets/rive/gift.riv');
       expect(Asset.satisfactory(), 'assets/rive/satisfactory.riv');
     },
