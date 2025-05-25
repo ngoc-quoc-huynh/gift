f00ecea5dd1bbe51a3c9dbcc589b84d868e44103
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-05-25 09:43:47 +0200

    Add satisfactory assets

--- gift_box/lib/static/resources/assets.dart
+++ gift_box/lib/static/resources/assets.dart
@@ -3,9 +3,22 @@ import 'package:gift_box/domain/models/asset.dart';
 final class Assets {
   const Assets._();
 
+  // Awesome shop items
+  static final ada = Asset('assets/awesome_shop/ada.webp');
+  static final ficsitCoupon = Asset('assets/awesome_shop/ficsit_coupon.webp');
+  static final ficsitCoffeeCup = Asset(
+    'assets/awesome_shop/ficsit_coffee_cup.webp',
+  );
+  static final darkMode = Asset('assets/awesome_shop/dark_mode.webp');
+  static final hardDrive = Asset('assets/awesome_shop/hard_drive.webp');
+  static final musicTape = Asset('assets/awesome_shop/music_tape.webp');
+  static final nobelisk = Asset('assets/awesome_shop/nobelisk.webp');
+
+  // Rive files
   static final gift = Asset('assets/rive/gift.riv');
   static final satisfactory = Asset('assets/rive/satisfactory.riv');
 
+  // Carousel images
   static final images = [
     Asset('assets/images/example_1.webp'),
     Asset('assets/images/example_2.webp'),
