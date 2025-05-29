a2f8d190f56d86d610eaa0423d1873df908962a6
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-05-29 11:59:22 +0200

    Add AwesomeShopItemConfirmationDialog

--- gift_box/lib/ui/widgets/coupon_display.dart
+++ gift_box/lib/ui/widgets/coupon_display.dart
@@ -1,37 +1,108 @@
+import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:gift_box/domain/models/asset.dart';
 import 'package:gift_box/domain/utils/extensions/build_context.dart';
 import 'package:gift_box/injector.dart';
 import 'package:gift_box/ui/widgets/asset_image.dart';
 
-class CouponDisplay extends StatelessWidget {
-  const CouponDisplay.large({required this.amount, super.key}) : height = 40;
+sealed class CouponDisplay extends StatelessWidget {
+  const CouponDisplay({required this.amount, super.key});
 
-  const CouponDisplay.small({required this.amount, super.key}) : height = 30;
+  const factory CouponDisplay.animated({
+    required int amount,
+    Key? key,
+  }) = _Animated;
+
+  const factory CouponDisplay.small({
+    required int amount,
+    Key? key,
+  }) = _Small;
+
+  const factory CouponDisplay.large({
+    required int amount,
+    Key? key,
+  }) = _Large;
 
   final int amount;
-  final double height;
+
+  @protected
+  static TranslationsWidgetsCouponDisplayEn get translations =>
+      Injector.instance.translations.widgets.couponDisplay;
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    required this.assetHeight,
+    required this.builder,
+  });
+
+  final double assetHeight;
+  final Widget Function(TextStyle?) builder;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
+      spacing: 10,
       children: [
         CustomAssetImage(
           asset: Asset.ficsitCoupon,
-          height: height,
+          height: assetHeight,
         ),
-        const SizedBox(width: 10),
-        Text(
-          _translations.couponAmount(n: amount),
-          style: context.textTheme.headlineLarge?.copyWith(
+        builder.call(
+          context.textTheme.headlineLarge?.copyWith(
             fontWeight: FontWeight.w600,
           ),
         ),
       ],
     );
   }
+}
 
-  static TranslationsWidgetsEn get _translations =>
-      Injector.instance.translations.widgets;
+class _Animated extends CouponDisplay {
+  const _Animated({required super.amount, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _Body(
+      assetHeight: 40,
+      builder: (style) => AnimatedFlipCounter(
+        value: amount,
+        prefix: '${CouponDisplay.translations.prefix} ',
+        textStyle: style,
+        curve: Curves.easeInOutQuad,
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+}
+
+class _Small extends CouponDisplay {
+  const _Small({required super.amount, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _Body(
+      assetHeight: 30,
+      builder: (style) => Text(
+        CouponDisplay.translations.amount(n: amount),
+        style: style,
+      ),
+    );
+  }
+}
+
+class _Large extends CouponDisplay {
+  const _Large({required super.amount, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _Body(
+      assetHeight: 40,
+      builder: (style) => Text(
+        CouponDisplay.translations.amount(n: amount),
+        style: style,
+      ),
+    );
+  }
 }
