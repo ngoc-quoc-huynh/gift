31f70673b182dd187f97eb546419a4c247c7499c
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-06-27 17:02:57 +0200

    Add SettingsPage

---
+++ gift_box/lib/ui/widgets/dialog/radio/dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gift_box/domain/blocs/value/cubit.dart';
+import 'package:gift_box/ui/widgets/dialog/action.dart';
+import 'package:gift_box/ui/widgets/dialog/dialog.dart';
+import 'package:gift_box/ui/widgets/dialog/radio/option.dart';
+
+class RadioDialog<T extends Object> extends StatelessWidget {
+  const RadioDialog({
+    required this.title,
+    required this.options,
+    super.key,
+  });
+
+  final String title;
+  final List<RadioDialogOption<T>> options;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomDialog.alert(
+      title: title,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: options,
+      ),
+      actions: [
+        const AlertDialogAction.cancel(),
+        AlertDialogAction.confirm(
+          result: () => context.read<ValueCubit<T>>().state,
+        ),
+      ],
+    );
+  }
+}
