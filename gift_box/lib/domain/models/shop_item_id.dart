8bd3bd270c142ccdea638cac8b8144efea784b2f
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-07-05 16:17:48 +0200

    Apply item effect after buy

--- gift_box/lib/domain/models/shop_item_id.dart
+++ gift_box/lib/domain/models/shop_item_id.dart
@@ -1,8 +1,26 @@
 enum ShopItemId {
-  ada,
-  darkMode,
-  germanDrive,
-  coffeeCup,
-  reset,
-  musicTape,
+  ada('ada'),
+  darkMode('dark-mode'),
+  germanDrive('german-drive'),
+  coffeeCup('coffee-cup'),
+  reset('reset'),
+  musicTape('music-tape');
+
+  const ShopItemId(this.id);
+
+  factory ShopItemId.byId(String id) => switch (id) {
+    'ada' => ada,
+    'dark-mode' => darkMode,
+    'german-drive' => germanDrive,
+    'coffee-cup' => coffeeCup,
+    'reset' => reset,
+    'music-tape' => musicTape,
+    String() => throw ArgumentError.value(
+      id,
+      'id',
+      'No enum value with that id.',
+    ),
+  };
+
+  final String id;
 }
