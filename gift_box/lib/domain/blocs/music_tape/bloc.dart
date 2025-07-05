69e2f65a2db8e2cb996e3263ec255eeee8d734ba
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-07-05 14:13:17 +0200

    Load assets for music tape and play it

--- gift_box/lib/domain/blocs/music_tape/bloc.dart
+++ gift_box/lib/domain/blocs/music_tape/bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter/foundation.dart';
+import 'package:gift_box/domain/utils/extensions/list.dart';
 import 'package:gift_box/injector.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:just_audio/just_audio.dart';
@@ -9,7 +10,7 @@ import 'package:just_audio/just_audio.dart';
 part 'event.dart';
 
 final class MusicTapeBloc extends HydratedBloc<MusicTapeEvent, bool> {
-  MusicTapeBloc() : _audioPlayer = Injector.instance.audioPlayer, super(false) {
+  MusicTapeBloc() : super(false) {
     on<MusicTapeInitializeEvent>(
       _onMusicTapeInitializeEvent,
       transformer: droppable(),
@@ -24,15 +25,17 @@ final class MusicTapeBloc extends HydratedBloc<MusicTapeEvent, bool> {
     );
   }
 
-  final AudioPlayer _audioPlayer;
+  static final _nativeApi = Injector.instance.nativeApi;
   static const _storageKey = 'music_tape';
+
+  AudioPlayer? _audioPlayer;
+  late final List<String> _assets;
 
   Future<void> _onMusicTapeInitializeEvent(
     MusicTapeInitializeEvent event,
     Emitter<bool> emit,
   ) async {
-    await _audioPlayer.setLoopMode(LoopMode.all);
-
+    _assets = await _nativeApi.loadMusicTape();
     if (state) {
       add(const MusicTapePlayEvent());
     }
@@ -42,8 +45,15 @@ final class MusicTapeBloc extends HydratedBloc<MusicTapeEvent, bool> {
     MusicTapePlayEvent event,
     Emitter<bool> emit,
   ) async {
-    await _audioPlayer.setAsset('assets/audios/ada/unlock.mp3');
-    unawaited(_audioPlayer.play());
+    await _disposeAudioPlayer();
+    _audioPlayer = AudioPlayer();
+    await [
+      _audioPlayer!.setLoopMode(LoopMode.all),
+      _audioPlayer!.setAudioSources(
+        _assets.map(AudioSource.asset).toList(growable: false)..shuffleSeeded(),
+      ),
+    ].wait;
+    unawaited(_audioPlayer!.play());
     emit(true);
   }
 
@@ -51,8 +61,13 @@ final class MusicTapeBloc extends HydratedBloc<MusicTapeEvent, bool> {
     MusicTapeStopEvent event,
     Emitter<bool> emit,
   ) async {
-    await _audioPlayer.stop();
+    await _disposeAudioPlayer();
     emit(false);
+  }
+
+  Future<void> _disposeAudioPlayer() async {
+    await _audioPlayer?.dispose();
+    _audioPlayer = null;
   }
 
   @override
@@ -65,7 +80,7 @@ final class MusicTapeBloc extends HydratedBloc<MusicTapeEvent, bool> {
 
   @override
   Future<void> close() async {
-    await _audioPlayer.dispose();
+    await _disposeAudioPlayer();
     return super.close();
   }
 }
