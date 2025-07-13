46bf04476d83b00f03871943ab9fd911efa645c0
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-07-13 15:54:36 +0200

    Migrate to OverlayPortal for WelcomeOverlay

--- gift_box/lib/ui/pages/shop_catalog/page.dart
+++ gift_box/lib/ui/pages/shop_catalog/page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gift_box/domain/blocs/hydrated_value/cubit.dart';
-import 'package:gift_box/domain/blocs/value/cubit.dart';
 import 'package:gift_box/domain/models/route.dart';
 import 'package:gift_box/domain/utils/extensions/build_context.dart';
 import 'package:gift_box/injector.dart';
@@ -10,7 +9,7 @@ import 'package:gift_box/ui/pages/shop_catalog/welcome.dart';
 import 'package:gift_box/ui/widgets/coupon_display.dart';
 import 'package:go_router/go_router.dart';
 
-class ShopCatalogPage extends StatefulWidget {
+class ShopCatalogPage extends StatelessWidget {
   const ShopCatalogPage({
     required this.navigationShell,
     required this.children,
@@ -21,81 +20,68 @@ class ShopCatalogPage extends StatefulWidget {
   final List<Widget> children;
 
   @override
-  State<ShopCatalogPage> createState() => _ShopCatalogPageState();
-}
-
-class _ShopCatalogPageState extends State<ShopCatalogPage> {
-  @override
-  void initState() {
-    super.initState();
-    if (!context.read<BoolCubit>().state) {
-      WidgetsBinding.instance.addPostFrameCallback(
-        (_) => WelcomeOverlay.show(context),
-      );
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          onPressed: () => context.pop(),
-        ),
-        title: Text(_translations.appBar),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: _translations.settingsTooltip,
-            onPressed: () => context.pushRoute(AppRoute.settings),
+    return WelcomeOverlay(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () => context.pop(),
           ),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: widget.navigationShell.currentIndex,
-        onDestinationSelected: (index) => widget.navigationShell.goBranch(
-          index,
-          initialLocation: widget.navigationShell.currentIndex == index,
-        ),
-        destinations: [
-          NavigationDestination(
-            icon: const Icon(Icons.local_offer_outlined),
-            selectedIcon: const Icon(Icons.local_offer_rounded),
-            label: _translations.navigationBar.specials,
-            tooltip: _translations.navigationBar.specials,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.palette_outlined),
-            selectedIcon: const Icon(Icons.palette_rounded),
-            label: _translations.navigationBar.customizer,
-            tooltip: _translations.navigationBar.customizer,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.local_cafe_outlined),
-            selectedIcon: const Icon(Icons.local_cafe_rounded),
-            label: _translations.navigationBar.equipment,
-            tooltip: _translations.navigationBar.equipment,
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.horizontalPadding,
+          title: Text(_translations.appBar),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: _translations.settingsTooltip,
+              onPressed: () => context.pushRoute(AppRoute.settings),
             ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: BlocBuilder<HydratedIntCubit, int>(
-                builder: (context, amount) =>
-                    CouponDisplay.animated(amount: amount),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: navigationShell.currentIndex,
+          onDestinationSelected: (index) => navigationShell.goBranch(
+            index,
+            initialLocation: navigationShell.currentIndex == index,
+          ),
+          destinations: [
+            NavigationDestination(
+              icon: const Icon(Icons.local_offer_outlined),
+              selectedIcon: const Icon(Icons.local_offer_rounded),
+              label: _translations.navigationBar.specials,
+              tooltip: _translations.navigationBar.specials,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.palette_outlined),
+              selectedIcon: const Icon(Icons.palette_rounded),
+              label: _translations.navigationBar.customizer,
+              tooltip: _translations.navigationBar.customizer,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.local_cafe_outlined),
+              selectedIcon: const Icon(Icons.local_cafe_rounded),
+              label: _translations.navigationBar.equipment,
+              tooltip: _translations.navigationBar.equipment,
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.horizontalPadding,
+              ),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: BlocBuilder<HydratedIntCubit, int>(
+                  builder: (context, amount) =>
+                      CouponDisplay.animated(amount: amount),
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: widget.children[widget.navigationShell.currentIndex],
-          ),
-        ],
+            Expanded(
+              child: children[navigationShell.currentIndex],
+            ),
+          ],
+        ),
       ),
     );
   }
