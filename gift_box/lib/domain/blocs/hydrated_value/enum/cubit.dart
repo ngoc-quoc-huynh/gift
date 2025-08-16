8bd12dff652838249a5feecf4a7bf3b22db91f8a
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-08-16 17:21:41 +0200

    Fix avoid_types_as_parameter_names

--- gift_box/lib/domain/blocs/hydrated_value/enum/cubit.dart
+++ gift_box/lib/domain/blocs/hydrated_value/enum/cubit.dart
@@ -1,19 +1,18 @@
 part of '../cubit.dart';
 
-final class HydratedEnumCubit<State extends Enum>
-    extends HydratedValueCubit<State> {
+final class HydratedEnumCubit<T extends Enum> extends HydratedValueCubit<T> {
   HydratedEnumCubit({
     required super.initialState,
     required super.storageKey,
     required this.values,
   });
 
-  final List<State> values;
+  final List<T> values;
 
   @override
-  State fromJson(Map<String, dynamic> json) =>
+  T fromJson(Map<String, dynamic> json) =>
       values.byName(json[storageKey] as String);
 
   @override
-  Map<String, dynamic>? toJson(State state) => {storageKey: state.name};
+  Map<String, dynamic>? toJson(T state) => {storageKey: state.name};
 }
