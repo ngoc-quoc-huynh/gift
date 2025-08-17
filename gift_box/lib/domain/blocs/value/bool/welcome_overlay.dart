e182cd894a3809e0d96a849b941a9ce9da9bc685
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-08-17 13:15:23 +0200

    Refactor AdaBloc
    - Migrate to OverlayPortal
    - Cancel previous audio, if new one is played

---
+++ gift_box/lib/domain/blocs/value/bool/welcome_overlay.dart
@@ -0,0 +1,6 @@
+part of '../cubit.dart';
+
+final class WelcomeOverlayCubit extends BoolCubit {
+  // ignore: avoid_positional_boolean_parameters, just controls visibility.
+  WelcomeOverlayCubit(super.initialState);
+}
