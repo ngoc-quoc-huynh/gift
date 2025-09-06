aa0cf40391ed9451acf5ea27fae582c1fb477d76
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-09-06 13:52:08 +0200

    Update countdown format to include days

--- gift_box/lib/domain/utils/extensions/duration.dart
+++ gift_box/lib/domain/utils/extensions/duration.dart
@@ -1,9 +1,11 @@
 extension DurationExtension on Duration {
-  String toHHMMSS() {
+  String toDDHHMMSS() {
+    final hours = inHours.remainder(24);
     final minutes = inMinutes.remainder(60);
     final seconds = inSeconds.remainder(60);
 
-    return '${inHours.toString().padLeft(2, '0')}:'
+    return '${inDays.toString().padLeft(2, '0')}:'
+        '${hours.toString().padLeft(2, '0')}:'
         '${minutes.toString().padLeft(2, '0')}:'
         '${seconds.toString().padLeft(2, '0')}';
   }
