b96093d3a6469b9a50a182551cd06b384f7a1931
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-10-24 15:52:34 +0200

    Migrate to kotlin dsl

---
+++ gift_keys/android/app/build.gradle.kts
@@ -0,0 +1,70 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keyProperties = Properties()
+val keyPropertiesFile = rootProject.file(".signing/key.properties")
+if (keyPropertiesFile.exists()) {
+    keyProperties.load(FileInputStream(keyPropertiesFile))
+}
+
+android {
+    namespace = "dev.huynh.gift.keys"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlin {
+        jvmToolchain(21)
+    }
+
+    defaultConfig {
+        applicationId = "dev.huynh.gift.keys"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keyProperties["keyAlias"] as String?
+            keyPassword = keyProperties["keyPassword"] as String?
+            storeFile = rootProject.file(".signing/keystore.jks")
+            storePassword = keyProperties["keyPassword"] as String?
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            ndk {
+                abiFilters.add("arm64-v8a")
+            }
+            isMinifyEnabled = true
+            isShrinkResources = true
+        }
+    }
+}
+
+base {
+    archivesName = "gift_keys_v${android.defaultConfig.versionName}"
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("androidx.window:window-core-android:1.5.0")
+}
